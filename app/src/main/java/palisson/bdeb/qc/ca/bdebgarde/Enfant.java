6871bde1837bf1f63f8a1f2a2ee8301e36538bb0
ÃAk0da <akh0dha@gmail.com>
2018-03-05 09:30:53 -0500

    Création de la classe enfant simplifiée.

--- app/src/main/java/palisson/bdeb/qc/ca/bdebgarde/Enfant.java
+++ app/src/main/java/palisson/bdeb/qc/ca/bdebgarde/Enfant.java
@@ -1,10 +1,19 @@
 package palisson.bdeb.qc.ca.bdebgarde;
 
+import java.util.Date;
+
 /**
  * Created by 1645720 on 2018-03-05.
  */
 
 public class Enfant {
     private String nom;
+    private Date dateNaissance;
+
+    public Enfant(String nom, Date dateNaissance){
+        this.nom = nom;
+        this.dateNaissance = dateNaissance;
+
+    }
 
 }
